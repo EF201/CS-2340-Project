b8e41fcc18ab2d28a1aaefa261de5b37884a7f5a
Cameron-Gai <92231513+Cameron-Gai@users.noreply.github.com>
2023-04-27 06:33:37 -0500

    Add files via upload

---
+++ Dots_and_Boxes_Split + Sound/main.asm
@@ -0,0 +1,40 @@
+.text
+.globl main
+
+main:
+    # Initialize and display the game board
+    jal init_board
+
+    # Game loop
+    game_loop:
+        # Display the game board
+        #jal display_board
+
+        # Take user input
+.globl user_score_point
+user_score_point:
+        jal take_user_input
+        
+        jal display_board
+        
+        jal box_checker
+
+        # Make a move for the computer
+.globl ai_score_point
+ai_score_point:
+        jal computer_move
+        
+	jal display_board
+	
+	jal box_checker_ai
+	
+        # Check game state
+        jal play_sound
+        
+        jal check_game_state
+        bnez $v0, game_loop
+    # Display the winner
+    jal display_winner
+
+    li $v0, 10
+    syscall
